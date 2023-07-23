b49941cce702503a578c5b2de0283dbce1dc34e2
Nicole <nicole.georgievva@gmail.com>
2023-07-23 16:37:18 +0300

    Solve StateExercise3

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    StateExercise2UI()
+                    StateExercise3UI()
                 }
             }
         }
---
+++ app/src/main/java/com/example/composeplayground/StateExercise3.kt
@@ -0,0 +1,61 @@
+package com.example.composeplayground
+
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StateExercise3UI() {
+    var isRegistered by rememberSaveable { mutableStateOf(false) }
+
+    if (!isRegistered) {
+        Column(modifier = Modifier.padding(12.dp)) {
+            var email by rememberSaveable { mutableStateOf("") }
+            var password by rememberSaveable { mutableStateOf("") }
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = if (it.contains("$")) it.dropLast(1) else it },
+                label = { Text("Email") }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") }
+            )
+
+            if (email.isNotBlank() && password.isNotBlank()) {
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Button(onClick = {
+                    isRegistered = true
+                }) {
+                    Text(text = "Register")
+                }
+            }
+        }
+    } else {
+        CircularProgressIndicator(
+            modifier = Modifier
+        )
+    }
+}
+
+
