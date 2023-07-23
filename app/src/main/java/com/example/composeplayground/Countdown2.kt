318dd95dfb72767442cb4fade46451e0f4a06ff7
Nicole <nicole.georgievva@gmail.com>
2023-07-23 17:49:11 +0300

    Improve Countdown2's UI

--- app/src/main/java/com/example/composeplayground/Countdown2.kt
+++ app/src/main/java/com/example/composeplayground/Countdown2.kt
@@ -1,5 +1,7 @@
 package com.example.composeplayground
 
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.runtime.LaunchedEffect
@@ -32,18 +34,33 @@ fun Countdown2() {
 fun CountdownOutput() {
     var text by rememberSaveable { mutableStateOf("5") }
     var countdownKey by rememberSaveable { mutableStateOf(0) }
+    var progressBar by rememberSaveable { mutableStateOf(1f) }
+
+    CircularProgressIndicator(
+        modifier = Modifier,
+        progress = progressBar
+    )
 
     LaunchedEffect(countdownKey) {
         delay(1000)
         text = "4..."
+        progressBar = 0.8f
+
         delay(1000)
         text = "3..."
+        progressBar = 0.6f
+
         delay(1000)
         text = "2..."
+        progressBar = 0.4f
+
         delay(1000)
         text = "1..."
+        progressBar = 0.2f
+
         delay(1000)
         text = "0"
+        progressBar = 0f
     }
 
     if (text == "0") {
@@ -52,6 +69,7 @@ fun CountdownOutput() {
         Button(onClick = {
             countdownKey++
             text = "5"
+            progressBar = 1f
         }) {
             Text(text = "Restart")
         }
