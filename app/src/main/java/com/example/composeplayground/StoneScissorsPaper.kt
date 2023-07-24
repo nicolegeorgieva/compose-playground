b3536d0347d97f20f8df47a3517d547423c2ebdd
Nicole <nicole.georgievva@gmail.com>
2023-07-24 15:03:16 +0300

    WIP: StoneScissorsPaper Game

--- app/src/main/java/com/example/composeplayground/StoneScissorsPaper.kt
+++ app/src/main/java/com/example/composeplayground/StoneScissorsPaper.kt
@@ -1,33 +1,33 @@
 package com.example.composeplayground
 
-import androidx.compose.foundation.layout.width
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.material3.RadioButton
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.TextRange
-import androidx.compose.material3.TextField
-import androidx.compose.foundation.layout.height
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StoneScissorsPaper() {
     var pressedStart by rememberSaveable { mutableStateOf(false) }
-    var selectedNumberOfUsers by rememberSaveable { mutableStateOf(false) }
+    var selectedNumberOfPlayers by rememberSaveable { mutableStateOf(false) }
+    var selectedPlayer1Name by rememberSaveable { mutableStateOf(false) }
 
     Column {
         if (!pressedStart) {
@@ -39,20 +39,20 @@ fun StoneScissorsPaper() {
                 Text(text = "Start")
             }
         } else {
-            if (!selectedNumberOfUsers) {
+            if (!selectedNumberOfPlayers) {
                 Text(text = "Choose number of players")
 
                 Spacer(modifier = Modifier.height(8.dp))
 
-                var state by rememberSaveable { mutableStateOf(true) }
+                var numberOfPlayers by rememberSaveable { mutableStateOf(true) }
 
                 Row(
                     modifier = Modifier.selectableGroup(),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     RadioButton(
-                        selected = state,
-                        onClick = { state = true },
+                        selected = numberOfPlayers,
+                        onClick = { numberOfPlayers = true },
                         modifier = Modifier.semantics { contentDescription = "Option 1" },
                         enabled = true
                     )
@@ -62,8 +62,8 @@ fun StoneScissorsPaper() {
                     Spacer(modifier = Modifier.width(8.dp))
 
                     RadioButton(
-                        selected = !state,
-                        onClick = { state = false },
+                        selected = !numberOfPlayers,
+                        onClick = { numberOfPlayers = false },
                         modifier = Modifier.semantics { contentDescription = "Option 2" },
                         enabled = true
                     )
@@ -73,11 +73,31 @@ fun StoneScissorsPaper() {
 
                 Spacer(modifier = Modifier.height(8.dp))
 
-                Button(onClick = { selectedNumberOfUsers = true }) {
-                    Text(text = "Continue")
+                if (numberOfPlayers) {
+                    Button(onClick = { selectedNumberOfPlayers = true }) {
+                        Text(text = "Continue")
+                    }
                 }
             } else {
-                Text(text = "Continue")
+                Text(text = "Set Player 1's name")
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                var player1Name by rememberSaveable { mutableStateOf("") }
+
+                TextField(
+                    value = player1Name,
+                    onValueChange = { player1Name = it },
+                    label = { Text("Player 1") }
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                if (player1Name.isNotBlank()) {
+                    Button(onClick = { selectedPlayer1Name = true }) {
+                        Text(text = "Continue")
+                    }
+                }
             }
         }
     }
