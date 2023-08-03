b8093d3598dd9e23ac9d220d2cb022c91df82d51
Nicole <nicole.georgievva@gmail.com>
2023-08-03 14:52:13 +0300

    WIP: GuessTheNumber Game

--- app/src/main/java/com/example/composeplayground/GuessTheNumber.kt
+++ app/src/main/java/com/example/composeplayground/GuessTheNumber.kt
@@ -8,32 +8,53 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlin.random.Random
 
 @Composable
 fun GuessTheNumber() {
-    StartOfTheGame()
-}
-
-@Composable
-fun StartOfTheGame() {
     Column(
         modifier = Modifier.padding(12.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Welcome to Guess The Number!")
+        var startIsPressed by rememberSaveable { mutableStateOf(false) }
 
-        Spacer(modifier = Modifier.height(12.dp))
-
-        ElevatedButton(
-            onClick = {
-                /* Do something! */
-            }
-        ) {
-            Text(text = "Start")
+        if (startIsPressed) {
+            GenerateInitialNumber()
+        } else {
+            StartOfTheGame(onStartPressed = { startIsPressed = true })
         }
     }
+}
+
+@Composable
+fun StartOfTheGame(onStartPressed: () -> Unit) {
+    Text(text = "Welcome to Guess The Number!")
+
+    Spacer(modifier = Modifier.height(12.dp))
+
+    ElevatedButton(
+        onClick = {
+            onStartPressed()
+        }
+    ) {
+        Text(text = "Start")
+    }
+}
+
+@Composable
+fun GenerateInitialNumber() {
+    val randomNumber = Random.nextInt(0, 100)
+
+    val generateInitialNumber by rememberSaveable { mutableStateOf(randomNumber) }
+
+    Text(text = "The first number is: $generateInitialNumber")
+
 }
