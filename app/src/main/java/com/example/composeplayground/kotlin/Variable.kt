0899aa1861920e3f2c8a04ef2fbd3aa7305fef10
Nicole <nicole.georgievva@gmail.com>
2023-08-08 16:23:54 +0300

    Refactor variable exercise

--- app/src/main/java/com/example/composeplayground/kotlin/Variable.kt
+++ app/src/main/java/com/example/composeplayground/kotlin/Variable.kt
@@ -1,7 +1,7 @@
 package com.example.composeplayground.kotlin
 
 fun main() {
-    val text = arrayListOf("Hello,")
+    val text = mutableListOf("Hello,")
     text.add("World!")
 
     println("${text[0]} ${text[1]}")
