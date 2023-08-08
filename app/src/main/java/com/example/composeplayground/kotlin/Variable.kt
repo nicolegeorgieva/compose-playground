9f3e4c0719e870cbf53eb317f9354326f3d9d137
Nicole <nicole.georgievva@gmail.com>
2023-08-08 16:29:37 +0300

    Solve variable exercise in a more functional way

--- app/src/main/java/com/example/composeplayground/kotlin/Variable.kt
+++ app/src/main/java/com/example/composeplayground/kotlin/Variable.kt
@@ -5,5 +5,10 @@ fun main() {
     text.add("World!")
 
     println("${text[0]} ${text[1]}")
+
+    val hello = listOf("Hello,")
+    val helloWorld = hello + listOf("World!")
+
+    println(helloWorld.joinToString(separator = " "))
 }
 
