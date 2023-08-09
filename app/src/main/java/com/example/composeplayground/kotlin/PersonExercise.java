53ecfca0f465e71a4144b4dd202848e26045aec6
Nicole <nicole.georgievva@gmail.com>
2023-08-09 18:09:01 +0300

    WIP: Java to Kotlin Conversion Exercise

---
+++ app/src/main/java/com/example/composeplayground/kotlin/PersonExercise.java
@@ -0,0 +1,13 @@
+package com.example.composeplayground.kotlin;
+
+public class PersonExercise {
+    private final String name;
+
+    public PersonExercise(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
