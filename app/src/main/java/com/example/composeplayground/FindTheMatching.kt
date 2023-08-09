615e298e017695c85bef8b0ce7cd1f589fc1d1f5
Nicole <nicole.georgievva@gmail.com>
2023-08-09 19:17:58 +0300

    WIP: Find the matching exercise

--- app/src/main/java/com/example/composeplayground/FindTheMatching.kt
+++ app/src/main/java/com/example/composeplayground/FindTheMatching.kt
@@ -1,19 +1,22 @@
 package com.example.composeplayground
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
@@ -27,20 +30,24 @@ fun FindTheMatching() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(text = "Welcome to find the matching game!")
+
+        Spacer(modifier = Modifier.height(8.dp))
+
         SquareItem(icon = Icons.Filled.Lock, contentDescription = "Lock")
     }
 }
 
 @Composable
 fun SquareItem(icon: ImageVector, contentDescription: String) {
-    FilledIconButton(
+    Button(
         modifier = Modifier
-            .size(64.dp)
-            .border(width = 2.dp, brush = Brush.linearGradient(), shape = RectangleShape),
+            .size(64.dp),
         onClick = {
             /* doSomething() */
         },
-        shape = RectangleShape
+        shape = RectangleShape,
+        border = BorderStroke(width = 2.dp, color = Color.Black)
     ) {
         Icon(
             imageVector = icon,
