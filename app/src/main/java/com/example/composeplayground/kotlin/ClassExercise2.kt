3eace12658fc91e70de341d2d825d02a8a0bdfde
Nicole <nicole.georgievva@gmail.com>
2023-08-14 18:33:56 +0300

    Exercise lateinit var in Class 2 Exercise

--- app/src/main/java/com/example/composeplayground/kotlin/ClassExercise2.kt
+++ app/src/main/java/com/example/composeplayground/kotlin/ClassExercise2.kt
@@ -2,6 +2,9 @@ package com.example.composeplayground.kotlin
 
 fun main() {
     val cat1 = Cat(name = "Amy", weight = 0.3, breed = "British")
+    cat1.temperament = "kind"
+
+    println(cat1.temperament)
 }
 
 class Cat(
@@ -9,6 +12,8 @@ class Cat(
     val weight: Double,
     val breed: String
 ) {
+    lateinit var temperament: String
+
     init {
         println("A $breed cat named $name was born. It weights $weight kg.")
     }
