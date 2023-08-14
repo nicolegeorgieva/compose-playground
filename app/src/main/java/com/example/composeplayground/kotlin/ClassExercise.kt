fc70f172e8f297d3552bc495891bd864286c892e
Nicole <nicole.georgievva@gmail.com>
2023-08-14 19:14:20 +0300

    Solve new variant of Class Exercise

--- app/src/main/java/com/example/composeplayground/kotlin/ClassExercise.kt
+++ app/src/main/java/com/example/composeplayground/kotlin/ClassExercise.kt
@@ -1,14 +1,39 @@
 package com.example.composeplayground.kotlin
 
 fun main() {
-    val dog1 = Dog(name = "Rex", weight = 50, breed = "German shepherd")
+    val dog1 = Dog(name = "Rex", weight_param = 100, breed_param = "german shepherd")
+    println(dog1.activities.toList().joinToString(" "))
+    dog1.weight = -2
+    println(dog1.weightInKgs)
     dog1.bark()
 }
 
 class Dog(
     val name: String,
-    val weight: Int,
-    val breed: String
+    val weight_param: Int,
+    val breed_param: String
 ) {
-    fun bark() = println(if (weight < 20) "Yip!" else "Woof!")
+    init {
+        println("Dog $name has been created. ")
+    }
+
+    var activities = arrayOf("Walks")
+
+    val breed = breed_param.uppercase()
+
+    init {
+        println("The breed is $breed.")
+    }
+
+    var weight = weight_param
+        set(value) {
+            if (value > 0) field = value
+        }
+
+    val weightInKgs: Double
+        get() = weight / 2.2
+
+    fun bark() {
+        println(if (weight < 20) "Yip!" else "Woof!")
+    }
 }
