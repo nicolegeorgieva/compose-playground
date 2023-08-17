35d98ac5efe00d5d79c2d5533878278066f9429c
Nicole <nicole.georgievva@gmail.com>
2023-08-17 14:02:27 +0300

    Solve Appliance Exercise

---
+++ app/src/main/java/com/example/composeplayground/kotlin/ApplianceExercise.kt
@@ -0,0 +1,33 @@
+package com.example.composeplayground.kotlin
+
+fun main() {
+    val coffeeMaker = CoffeeMaker()
+
+    println(coffeeMaker.pluggedIn)
+    coffeeMaker.fillWithWater()
+    coffeeMaker.consumePower()
+}
+
+abstract class Appliance() {
+    var pluggedIn = true
+    abstract val color: String
+
+    abstract fun consumePower()
+}
+
+class CoffeeMaker : Appliance() {
+    override val color = ""
+    var coffeeLeft = false
+
+    override fun consumePower() {
+        println("Consuming power.")
+    }
+
+    fun fillWithWater() {
+        println("Fill with water.")
+    }
+
+    fun makeCoffee() {
+        println("Make the coffee.")
+    }
+}
